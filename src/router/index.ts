b0a537470b96c6f050dfec497bbde4b80b45287d
m-miyawaki <miyawakifamilymyam@gmail.com>
2026-06-01 00:00:52 +0900

    feat: add Item detail page (edit/delete/404)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import ItemListPage from '@/views/ItemListPage.vue'
+import ItemDetailPage from '@/views/ItemDetailPage.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/items' },
   { path: '/items', component: ItemListPage },
+  { path: '/items/:id', component: ItemDetailPage },
 ]
 
 export const router = createRouter({
